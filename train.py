7fc376717be0bf4cce03ad5f7424ad462b6ba7da
Ryan O'Connor <ryan.spencer.oconnor@gmail.com>
2022-08-04 20:03:38 -0400

    Made train.py more robust

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import json
+import os
 import sys
+from argparse import ArgumentParser
 from concurrent.futures import ThreadPoolExecutor
+from contextlib import contextmanager
 from datetime import datetime
 from functools import partial
 import io
@@ -8,7 +11,6 @@ import urllib
 from typing import Literal
 
 import datasets
-import nonechucks as nc
 import PIL.Image
 from einops import rearrange
 import torch.utils.data
@@ -25,7 +27,96 @@ from minimagen.Unet import Unet
 from minimagen.helpers import exists
 from minimagen.t5 import get_encoded_dim, t5_encode_text
 
+
+# TODO: ADD LOGGING THAT KEEPS TRACK OF TRAINING/VALID LOSSES AND TESTING LOSS
+
 USER_AGENT = get_datasets_user_agent()
+
+
+class MinimagenDataset(torch.utils.data.Dataset):
+
+    def __init__(self, hf_dataset, *, encoder_name, max_length, train=True, img_transform=None):
+        """
+        MinImagen Dataset
+
+        :param hf_dataset: HuggingFace `datasets` Dataset object
+        :param encoder_name: Name of the T5 encoder to use.
+        :param max_length: Maximum number of words allowed in a given caption.
+        :param train: Whether train or test dataset
+        :param img_transform: (optional) Transforms to be applied on a sample
+        """
+
+        split = "train" if train else "validation"
+
+        self.urls = hf_dataset[f"{split}"]['image_url']
+        self.captions = hf_dataset[f"{split}"]['caption']
+
+        self.img_transform = img_transform
+        self.encoder_name = encoder_name
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.urls)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img = fetch_single_image(self.urls[idx])
+        if img is None:
+            return None
+        elif self.img_transform:
+            img = self.img_transform(img)
+        if img.shape[0] != 3:
+            return None
+
+        enc, msk = t5_encode_text([self.captions[idx]], self.encoder_name, self.max_length)
+
+        return {'image': img, 'encoding': enc, 'mask': msk}
+
+
+class Rescale:
+    """
+    Transformation to scale images to the proper size
+    """
+
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def __call__(self, sample, *args, **kwargs):
+        if len(sample.shape) == 2:
+            sample = rearrange(sample, 'h w -> 1 h w')
+        elif not len(sample.shape) == 3:
+            raise ValueError("Improperly shaped image for rescaling")
+
+        return resize_image_to_square(sample, self.side_length)
+
+
+def collate(batch):
+    # Filter out None instances or those in which the image could not be fetched
+    batch = list(filter(lambda x: x is not None, batch))
+    batch = list(filter(lambda x: x['image'] is not None, batch))
+
+    # Expand mask and encodings to len of elt in batch with greatest number of words
+    max_len = max([batch[i]['mask'].shape[1] for i in range(len(batch))])
+
+    for elt in batch:
+        length = elt['mask'].shape[1]
+        rem = max_len - length
+        elt['mask'] = torch.squeeze(elt['mask'])
+        elt['encoding'] = torch.squeeze(elt['encoding'])
+        if rem > 0:
+            elt['mask'] = F.pad(elt['mask'], (0, rem), 'constant', 0)
+            elt['encoding'] = F.pad(elt['encoding'], (0, 0, 0, rem), 'constant', False)
+
+    return torch.utils.data.dataloader.default_collate(batch)
+
+
+def fetch_images(batch, num_threads, timeout=None, retries=0):
+    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
+    return batch
 
 
 def fetch_single_image(image_url, timeout=None, retries=0):
@@ -43,33 +134,6 @@ def fetch_single_image(image_url, timeout=None, retries=0):
             image = None
     return image
 
-
-def fetch_images(batch, num_threads, timeout=None, retries=0):
-    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
-    return batch
-
-class Rescale:
-
-    def __init__(self, side_length):
-        self.side_length = side_length
-
-    def __call__(self, sample, *args, **kwargs):
-        '''
-        if len(sample.shape) == 2:
-            sample = rearrange(sample, 'h w -> 1 1 h w')
-        elif len(sample.shape) == 3:
-            sample = rearrange(sample, 'c h w -> 1 c h w')
-        elif not len(sample.shape) == 4:
-            raise ValueError("Improperly shaped image for rescaling")
-        '''
-        if len(sample.shape) == 2:
-            sample = rearrange(sample, 'h w -> 1 h w')
-        elif not len(sample.shape) == 3:
-            raise ValueError("Improperly shaped image for rescaling")
-
-        return resize_image_to_square(sample, self.side_length)
 
 def resize_image_to_square(image: torch.tensor,
                     target_image_size: int,
@@ -100,88 +164,88 @@ def resize_image_to_square(image: torch.tensor,
 
     return out
 
+def create_directory(dir_path):
+    """
+    Creates a directory at the given path if it does not exist already and returns a context manager that allows user
+        to temporarily enter the direcory
+    """
+    original_dir = os.getcwd()
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        os.makedirs(os.path.join(dir_path, "parameters"))
+        os.makedirs(os.path.join(dir_path, "state_dicts"))
 
-class MinimagenDataset(torch.utils.data.Dataset):
+    @contextmanager
+    def cm(subpath=""):
+        os.chdir(os.path.join(dir_path, subpath))
+        yield
+        os.chdir(original_dir)
+    return cm
 
-    def __init__(self, hf_dataset, *, encoder_name, max_length, train = True, img_transform = None):
-        """
-        MinImagen Dataset
+# Training timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-        :param hf_dataset: HuggingFace `datasets` Dataset object
-        :param train: Whether train or test dataset
-        :param transform: (optional) Transforms to be applied on a sample
-        """
+# Create training directory
+dir_path = f"./training_{timestamp}"
+training_dir = create_directory(dir_path)
 
-        split = "train" if train else "validation"
-
-        self.urls = hf_dataset[f"{split}"]['image_url']
-        self.captions = hf_dataset[f"{split}"]['caption']
-        # TODO: Memory not large enough to store entire encoding (~434 GB), but calculating encoding on the fly
-        #   too slow? Does it push through each image in series or does DataLoader make them a batch
-        #   Also have to recalculate each epoch which is slower
-        #self.encoding, self.mask = t5_encode_text(hf_dataset[f"{split}"]['caption'], encoder_name, max_length)
-        self.img_transform = img_transform
-        self.encoder_name = encoder_name
-        self.max_length = max_length
-
-    def __len__(self):
-        return len(self.urls)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img = fetch_single_image(self.urls[idx])
-        if img is None:
-            return None
-        elif self.img_transform:
-            img = self.img_transform(img)
-        if img.shape[0] != 3:
-            return None
-
-        print(self.captions[idx])
-        encoding, mask = t5_encode_text([self.captions[idx]], self.encoder_name, self.max_length)
-
-        return {'image': img, 'encoding': encoding, 'mask': mask}
-
-def collate(batch):
-    # Filter out None instances or those in which the image could not be fetched
-    batch = list(filter(lambda x: x is not None, batch))
-    batch = list(filter(lambda x: x['image'] is not None, batch))
-
-    # Expand mask and encodings to len of elt in batch with greatest number of words
-    max_len = max([batch[i]['mask'].shape[1] for i in range(len(batch))])
-
-    for elt in batch:
-        length = elt['mask'].shape[1]
-        rem = max_len - length
-        elt['mask'] = torch.squeeze(elt['mask'])
-        elt['encoding'] = torch.squeeze(elt['encoding'])
-        if rem > 0:
-            elt['mask'] = F.pad(elt['mask'], (0, rem), 'constant', 0)
-            elt['encoding'] = F.pad(elt['encoding'], (0, 0, 0, rem), 'constant', False)
-
-    return torch.utils.data.dataloader.default_collate(batch)
-
-# Constants
-BATCH_SIZE = 4  # Batch size training data
-MAX_NUM_WORDS = 64  # Max number of words allowed in a caption
-IMG_SIDE_LEN = 128  # Side length of the training images/final output image from Imagen
-EPOCHS = 1  # Number of epochs to train from
-T5_NAME = "t5_small"  # Name of the T5 encoder to use
-TRAIN_VALID_FRAC = 0.5 # TODO: Change to 0.8
-TIMESTEPS = 250
-OPTIM_LR = 0.0001
+# Optionally hard-code values (will overwrite command-line)
+BATCH_SIZE = None
+MAX_NUM_WORDS = None
+IMG_SIDE_LEN = None
+EPOCHS = None
+T5_NAME = None
+TRAIN_VALID_FRAC = None
+TIMESTEPS = None
+OPTIM_LR = None
 TESTING = True
+
+# Command line argument parser
+parser = ArgumentParser()
+parser.add_argument("-b", "--BATCH_SIZE", dest="BATCH_SIZE", help="Batch size", default=8)
+parser.add_argument("-mw", "--MAX_NUM_WORDS", dest="MAX_NUM_WORDS", help="Maximum number of words allowed in a caption", default=64)
+parser.add_argument("-s", "--IMG_SIDE_LEN", dest="IMG_SIDE_LEN", help="Side length of square Imagen output images", default=128)
+parser.add_argument("-e", "--EPOCHS", dest="EPOCHS", help="Number of training epochs", default=5)
+parser.add_argument("-t5", "--T5_NAME", dest="T5_NAME", help="Name of T5 encoder to use", default='t5_small')
+parser.add_argument("-f", "--TRAIN_VALID_FRAC", dest="TRAIN_VALID_FRAC", help="Fraction of dataset to use for training (vs. validation)", default=0.8)
+parser.add_argument("-t", "--TIMESTEPS", dest="TIMESTEPS", help="Number of timesteps in Diffusion process", default=1000)
+parser.add_argument("-lr", "--OPTIM_LR", dest="OPTIM_LR", help="Learning rate for Adam optimizer", default=0.0001)
+parser.add_argument("-test", "--TESTING", dest="TESTING", help="Whether to test with smaller dataset", default=False)
+args = parser.parse_args()
+
+# For each command-line argument, replace with hard-coded value if it exists
+for i in args.__dict__.keys():
+    if vars()[i] is not None and vars()[i] != getattr(args, i):
+        print(f"\nWARNING: {i} defaulting to hard-coded value of {vars()[i]} rather than the value of {getattr(args, i)} "
+              f"supplied by a (potentially default) command-line argument. Edit `./train.py` to remove this value.\n")
+    else:
+        vars()[i] = getattr(args, i)
+
+# Save the training parameters if not testing
+if not TESTING:
+    with training_dir("parameters"):
+        with open(f"training_parameters_{timestamp}.txt", "w") as f:
+            for i in args.__dict__.keys():
+                f.write(f'--{i}={vars()[i]}\n')
+
 
 # Get encoding dimension of the text encoder
 text_embed_dim = get_encoded_dim(T5_NAME)
 
 # HuggingFace dataset
 dset = load_dataset("conceptual_captions")
-print(sys.getsizeof(dset))
-# Cut down size for testing
+
+# If testing, lower parameter values for lower computational load
 if TESTING:
+    BATCH_SIZE = 4
+    MAX_NUM_WORDS = 32
+    IMG_SIDE_LEN = 128
+    EPOCHS = 2
+    T5_NAME = 't5_small'
+    TRAIN_VALID_FRAC = 0.5
+    TIMESTEPS = 25  # Do not make less than 20
+    OPTIM_LR = 0.0001
+
     num = 16
     vi = dset['validation']['image_url'][:num]
     vc = dset['validation']['caption'][:num]
@@ -194,12 +258,10 @@ if TESTING:
         'validation':{
             'image_url': ti,
             'caption': tc,}, 'num_rows':num}
-    print(dset['train']['caption'])
 
 # Torch train/valid dataset
 dataset_train_valid = MinimagenDataset(dset, max_length=MAX_NUM_WORDS, encoder_name=T5_NAME, train=True,
                                        img_transform=Compose([ToTensor(), Rescale(IMG_SIDE_LEN)]))
-print(sys.getsizeof(dataset_train_valid))
 
 # Split into train/valid
 train_size = int(TRAIN_VALID_FRAC * len(dataset_train_valid))
@@ -298,15 +360,14 @@ print("Created Imagen")
 optimizer = optim.Adam(imagen.parameters(), lr=OPTIM_LR)
 print("Created optimzer")
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
 # Save parameters
-with open(f'base_params_{timestamp}.json', 'w') as f:
-    f.write(json.dumps(base_unet_params))
-with open(f'super_params_{timestamp}.json', 'w') as f:
-    f.write(json.dumps(super_res_params))
-with open(f'imagen_params_{timestamp}.json', 'w') as f:
-    f.write(json.dumps(imagen_params))
+with training_dir("parameters"):
+    with open(f'base_params_{timestamp}.json', 'w') as f:
+        json.dump(base_unet_params, f, indent=4)
+    with open(f'super_params_{timestamp}.json', 'w') as f:
+        json.dump(super_res_params, f, indent=4)
+    with open(f'imagen_params_{timestamp}.json', 'w') as f:
+        json.dump(imagen_params, f, indent=4)
 
 # Train
 best_loss = [9999999 for i in range(len(unets))]
@@ -349,8 +410,9 @@ for epoch in range(EPOCHS):
         print(f"Unet {i} avg validation loss: ", l)
         if l < best_loss[i]:
             best_loss[i] = l
-            model_path = f"imagen_{timestamp}_{i}_{epoch+1}_{l:.3f}.pth"
-            torch.save(imagen.unets[i].state_dict(), model_path)
+            with training_dir("state_dicts"):
+                model_path = f"imagen_{timestamp}_{i}_{epoch+1}_{l:.3f}.pth"
+                torch.save(imagen.unets[i].state_dict(), model_path)
 
 
 # Generate images with "trained" model
