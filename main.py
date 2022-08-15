1401858c7bb9380babbf712d87f3dc361848a097
Ryan O'Connor <ryan.spencer.oconnor@gmail.com>
2022-08-15 17:29:49 -0400

    Ready for publish

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import subprocess
 from datetime import datetime
 
+# Get timestamp for training
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Run training on small test Imagen
 subprocess.call(["venv/Scripts/python", "train.py", "-test", "-ts", timestamp])
+
+# Use small test Imagen to generate image
 subprocess.call(["venv/Scripts/python", "inference.py", "-d", f"training_{timestamp}"])
