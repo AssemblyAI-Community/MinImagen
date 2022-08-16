49a789a3fc5794d8127818c66396b9b4499b1545
Ryan O'Connor <ryan.spencer.oconnor@gmail.com>
2022-08-16 11:05:57 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.5'
-DESCRIPTION = 'Minimal Imagen text-to-image model implementation. Includes '
+DESCRIPTION = 'Minimal Imagen text-to-image model implementation.'
+LONG_DESCRIPTION = "Minimal Imagen text-to-image model implementation. See the [GitHub repo](https://github.com/AssemblyAI-Examples/MinImagen) or the [how-to build guide](www.assemblyai.com/blog/build-your-own-imagen-text-to-image-model/) for more details"
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -13,6 +14,7 @@ setup(
     author="AssemblyAI",
     author_email="<ryan@assemblyai.com>",
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=required,
     keywords=[ 'imagen',
