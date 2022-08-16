9919eac1446cab79eacc9f88b0733edcd3697fc9
Ryan O'Connor <ryan.spencer.oconnor@gmail.com>
2022-08-16 16:26:41 -0400

    More succinct

--- train.py
+++ train.py
@@ -6,12 +6,12 @@ from torch import optim
 
 
 from minimagen.Imagen import Imagen
-from minimagen.Unet import Unet, Base, Super
+from minimagen.Unet import Unet, Base, Super, BaseTest, SuperTest
 from minimagen.generate import load_minimagen, load_params
 from minimagen.t5 import get_encoded_dim
 from minimagen.training import get_minimagen_parser, ConceptualCaptions, get_minimagen_dl_opts, \
     create_directory, get_model_params, get_model_size, save_training_info, get_default_args, MinimagenTrain, \
-    load_restart_training_parameters
+    load_restart_training_parameters, load_testing_parameters
 
 # Get device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -34,23 +34,11 @@ training_dir = create_directory(dir_path)
 
 # If resuming a training, make sure certain cmd line arguments align and reflect the MinImagen instance being loaded
 if args.RESTART_DIRECTORY is not None:
-    args.__dict__ = {**args.__dict__, **load_restart_training_parameters(args.RESTART_DIRECTORY)}
+    args = load_restart_training_parameters(args)
 
 # If testing, lower parameter values to lower computational load and also to lower amount of data being used.
 if args.TESTING:
-    args.__dict__ = {
-        **args.__dict__,
-        **dict(
-            BATCH_SIZE=2,
-            MAX_NUM_WORDS=32,
-            IMG_SIDE_LEN=128,
-            EPOCHS=2,
-            T5_NAME='t5_small',
-            TRAIN_VALID_FRAC=0.5,
-            TIMESTEPS=25,  # Do not make less than 20
-            OPTIM_LR=0.0001
-        )
-    }
+    args = load_testing_parameters(args)
     train_dataset, valid_dataset = ConceptualCaptions(args, smalldata=True)
 else:
     train_dataset, valid_dataset = ConceptualCaptions(args, smalldata=False)
@@ -65,71 +53,24 @@ text_embed_dim = get_encoded_dim(args.T5_NAME)
 
 # Create Unets
 if args.RESTART_DIRECTORY is None:
+    imagen_params = dict(
+        image_sizes=(int(args.IMG_SIDE_LEN / 2), args.IMG_SIDE_LEN),
+        timesteps=args.TIMESTEPS,
+        cond_drop_prob=0.15,
+        text_encoder_name=args.T5_NAME
+    )
+
     # If not loading a training from a checkpoint
     if args.TESTING:
         # If testing, use tiny MinImagen for low computational load
-        base_params = dict(
-            dim=8,
-            text_embed_dim=text_embed_dim,
-            cond_dim=8,
-            dim_mults=(1, 2),
-            num_resnet_blocks=1,
-            layer_attns=(False, False),
-            layer_cross_attns=(False, False),
-            attend_at_middle=False
-        )
+        unets_params = [get_default_args(BaseTest), get_default_args(SuperTest)]
 
-        super_params = dict(
-            dim=8,
-            text_embed_dim=text_embed_dim,
-            cond_dim=8,
-            dim_mults=(1, 2),
-            num_resnet_blocks=(1, 2),
-            layer_attns=(False, False),
-            layer_cross_attns=(False, False),
-            attend_at_middle=False
-        )
-
-        unets_params = [base_params, super_params]
-
-        imagen_params = dict(
-            image_sizes=(int(args.IMG_SIDE_LEN/2), args.IMG_SIDE_LEN),
-            timesteps=args.TIMESTEPS,
-            cond_drop_prob=0.15,
-            text_encoder_name=args.T5_NAME
-        )
-
-        imagen_params = {**get_default_args(Imagen), **imagen_params}
     # Else if not loading Unet/Imagen settings from a config (parameters) folder, use defaults
     elif not args.PARAMETERS:
         # If no parameters provided, use params from minimagen.Imagen.Base and minimagen.Imagen.Super built-in classes
-        base_params = dict(
-            dim=512,
-            dim_mults=(1, 2, 3, 4),
-            num_resnet_blocks=3,
-            layer_attns=(False, True, True, True),
-            layer_cross_attns=(False, True, True, True),
-            memory_efficient=False,
-            text_embed_dim=text_embed_dim
-        )
-        super_params = defaults = dict(
-            dim=128,
-            dim_mults=(1, 2, 4, 8),
-            num_resnet_blocks=(2, 4, 8, 8),
-            layer_attns=(False, False, False, True),
-            layer_cross_attns=(False, False, False, True),
-            memory_efficient=True,
-            text_embed_dim=text_embed_dim
-        )
+        unets_params = [get_default_args(Base), get_default_args(Super)]
 
-        unets_params = [base_params, super_params]
-
-        imagen_params = dict(
-            image_sizes=(int(args.IMG_SIDE_LEN/2), args.IMG_SIDE_LEN),
-            timesteps=args.TIMESTEPS,
-            text_encoder_name=args.T5_NAME
-        )
-    # Else load unet/Imagen configs from config (parameters) folder
+    # Else load unet/Imagen configs from config (parameters) folder (override imagen+params)
     else:
         # If parameters are provided, load them
         unets_params, imagen_params = get_model_params(args.PARAMETERS)
