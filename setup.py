9323b74d6bc2ee07676218763b414c0e75ecabab
Ryan O'Connor <ryan.spencer.oconnor@gmail.com>
2022-08-16 14:53:50 -0400

    v0.0.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'Minimal Imagen text-to-image model implementation.'
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
